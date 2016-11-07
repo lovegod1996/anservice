f2e4c53a5d00cab66b9c89715ac077ca05e113be
Lovegod <dx96_j@163.com>
2016-11-07 17:00:19 +0800

    添加commonutil，生成json字符串

--- src/com/a123/an/servlet/HelpServlet.java
+++ src/com/a123/an/servlet/HelpServlet.java
@@ -1,11 +1,20 @@
 package com.a123.an.servlet;
 
+import com.a123.an.dao.HelpDaoImpl;
+import com.a123.an.util.CommonUtil;
+import net.sf.json.JSONObject;
+import org.apache.commons.lang.SystemUtils;
+
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Author: lovegod
@@ -30,6 +39,19 @@ public class HelpServlet extends HttpServlet {
             e.printStackTrace();
         }
         //数据库操作
+        HelpDaoImpl daoimpl=new HelpDaoImpl();
+        List<Map<String,Object>> helpList=daoimpl.getHelpList(version);
+        Map<String,Object> data=new HashMap<String,Object>();
+         data.put("response","help");
+        data.put("version","12");
+        data.put("helplist",helpList);
+
+
+     /*   JSONObject object=JSONObject.fromObject(data);
+        System.out.println(object.toString());
+        response.getWriter().write(object.toString());
+        response.getWriter().flush();*/
+        CommonUtil.renderJson(response,data);
 
     }
 
